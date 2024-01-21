73c54e9f191128fe2eaee62b69a8e7c0e725b908
Nikhil Maharishi <nikhilmaharishi5@gmail.com>
2024-01-21 19:51:15 +0530

    feat: created experience,about, contact,footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LandingComponent } from './landing/landing.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LandingComponent
+    LandingComponent,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,SkillsComponent,ExperienceComponent,AboutComponent,ContactComponent,FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
