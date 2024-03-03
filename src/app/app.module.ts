f2c7bc237af22407cc5c8bf69b775557c6fd0a13
Nikhil Maharishi <nikhilmaharishi5@gmail.com>
2024-03-03 22:25:05 +0530

    fix:ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,9 @@ import { RouterModule } from '@angular/router';
     LandingComponent,
     RouterModule,
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
